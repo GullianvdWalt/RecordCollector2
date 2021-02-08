657fcd4f7526dec3644cba200788c7190119f729
Gullian <gullianvdwalt1@gmail.com>
2021-02-08 11:45:15 +0200

    init

---
+++ src/main/java/com/gvdw/record_collector/RecordCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.gvdw.record_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordCollectorApplication.class, args);
+	}
+
+}
